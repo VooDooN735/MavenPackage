3ce9d7ada4793ff85489c3aa9cbb1b5041ba44ea
VooDooN735 <107447207+VooDooN735@users.noreply.github.com>
2022-08-15 17:38:58 +0300

    MavenPac

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.sqr;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+    @Test
+    public void test() {
+        SQRService service = new SQRService();
+        int actual = service.calcSqrMinMax(200, 300);
+        int expected = 3;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+
+}
